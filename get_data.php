6842701a3b4b2fbfafe628e6b33165fb356f61d6
cole-davison <122580230+cole-davison@users.noreply.github.com>
2025-02-12 16:06:13 -0500

    Update

--- get_data.php
+++ get_data.php
@@ -2,18 +2,7 @@
 header("Access-Control-Allow-Origin: *"); 
 header("Content-Type: application/json; charset=UTF-8");
 
-$servername = "your_server";
-$username = "your_username";
-$password = "your_password";
-$database = "your_database";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $database);
-
-// Check connection
-if ($conn->connect_error) {
-    die(json_encode(["error" => "Connection failed: " . $conn->connect_error]));
-}
+include "db.php";
 
 $sql = "SELECT id, email, password, role,  first_name, last_name, avatar_url, profile_bio, created_at FROM users";
 $result = $conn->query($sql);
